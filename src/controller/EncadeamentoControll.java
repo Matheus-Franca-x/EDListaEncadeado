14d8f2547594e185ee42b189d47b81c7bfe02d41
Matheus <matheussss6613@gmail.com>
2023-05-04 00:21:21 -0300

    Fim, porém falta informação no exercício 3

---
+++ src/controller/EncadeamentoControll.java
@@ -0,0 +1,70 @@
+package controller;
+
+import model.ListaInt;
+
+public class EncadeamentoControll {
+	
+	public EncadeamentoControll() 
+	{
+		super();
+	}
+	
+	public void criaList(int[] vet, ListaInt lista)
+	{
+		
+		for (int each: vet)
+		{
+			if(lista.isEmpty())
+			{
+				lista.addFirst(each);
+			}
+			else
+			{
+				try {
+					lista.addLast(each);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	public void listEncadeado(ListaInt lista)
+	{
+		while(lista.size() < 14)
+		{
+			int rand = (int) (Math.random() * 14) + 1;
+			boolean test = true;
+			try {
+				for (int i = 0; i < lista.size(); i++)
+				{
+					if(rand == lista.get(i))
+					{
+						test = false;
+					}
+				}
+				if(test)
+				{
+					lista.addLast(rand);					
+				}
+				else if (rand % 2 == 0)
+				{
+					lista.add(rand, 2);
+				}
+				else
+				{
+					lista.add(rand, 1);					
+				}
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,33 @@
+package view;
+
+import controller.EncadeamentoControll;
+import model.ListaInt;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		ListaInt list = new ListaInt();
+		EncadeamentoControll controll = new EncadeamentoControll();
+		int vet[] = {1, 2, 6, 7, 8};
+		
+		
+		controll.criaList(vet, list);
+		
+		controll.listEncadeado(list);
+		
+		
+		while(!list.isEmpty())
+		{
+			try {
+				System.out.println(list.get(0));
+				list.remove(0);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+
+}
